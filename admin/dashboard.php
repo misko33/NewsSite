05bda4b3244e907d03bc4cd120476c65aa2bffb7
Dorian Slade Silovic <doriansladesilovic@infoscope.hr>
2022-06-24 12:50:02 +0200

    added css to home page + dashboard sql order by date

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,39 +1,41 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-12">
-            <h2>
-                Login korisnika
-            </h2>
-            <?php  $log = $korisnik->all_log_auth();
+          <h2 class="titleLogin">
+              Login korisnika
+          </h2>
+          <?php  $log = $korisnik->all_log_auth();
 
-            if(!empty($log)) { ?>
-          <table id="lista_korisnika" class="table table-striped table-bordered" style="width:100%">
-            <thead style="text-align: center">
+          if(!empty($log)) { ?>
+          <div class="authTable">
+            <table class="table table-striped table-bordered" style="width:100%">
+              <thead style="text-align: center">
+                <tr style="text-align: center">
+                  <th style="text-align: center">User</th>
+                  <th class="text-center">IP</th>
+                  <th class="text-center">Datum</th>
+                  <th class="text-center">Login</th>
+                  <th>User Agent</th>
+                </tr>
+              </thead>
+              <tbody style="text-align: center">
+              <?php foreach ($log as $log1) : ?>
               <tr style="text-align: center">
-                <th style="text-align: center">User</th>
-                <th class="text-center">IP</th>
-                <th class="text-center">Datum</th>
-                <th class="text-center">Login</th>
-                <th>User Agent</th>
+                <td style="text-align: center"><?php echo $log1['username']; ?></td>
+                <td class="text-center"><?php echo $log1['ip']; ?></td>
+                <td class="text-center"><?php echo $log1['datum']; ?></td>
+                <td class="text-center"><?php echo $log1['islogin'] ? 'True' : 'False'; ?></td>
+                <td><?php echo $log1['useragent']; ?></td>
               </tr>
-            </thead>
-            <tbody style="text-align: center">
-            <?php foreach ($log as $log1) : ?>
-            <tr style="text-align: center">
-              <td style="text-align: center"><?php echo $log1['username']; ?></td>
-              <td class="text-center"><?php echo $log1['ip']; ?></td>
-              <td class="text-center"><?php echo $log1['datum']; ?></td>
-              <td class="text-center"><?php echo $log1['islogin'] ? 'True' : 'False'; ?></td>
-              <td><?php echo $log1['useragent']; ?></td>
-            </tr>
-            <?php endforeach; ?>
-            </tbody>
-          </table>  
+              <?php endforeach; ?>
+              </tbody>
+            </table> 
+          </div> 
           <?php
-        } 
-        else {
-          echo "Trenutno nema podataka o login-u";
-        } ?>
+          } 
+          else {
+            echo "Trenutno nema podataka o login-u";
+          } ?>
         </div>    
     </div>    
 </div>
--- home.php
+++ home.php
@@ -6,17 +6,19 @@ $br_clanaka = 3;
     <div class="section-content">
         <ul class="articles">
 			<div class="section-heading">
-				<h2 class="section-title text"><a href="?page=clanci&kat=politika" class="heading-link">Politik</a></h2>
+				<h2 class="section-title"><a href="?page=clanci&kat=politika" class="heading-link">Politik</a></h2>
 			</div>
 			<?php 
 				$clanci = $clanak->getClanci('politika'); 
 
 				if(!empty($clanci)) {
 					$count = 0; 
-					foreach ($clanci as $vijest) { 
-						echo "<li>" ;
+					foreach ($clanci as $vijest) {
+						$clanak_url = "?page=clanci&kat=" . $vijest['kategorija'] . "&cid=" . $vijest['id'];
+						echo "<li id='" . $vijest['kategorija'] . $vijest['id'] ."' style='cursor: pointer'>";
 						$clanak->ispis_clanka_front($vijest);
 						echo "</li>";
+						echo '<script>document.getElementById("' . $vijest['kategorija'] . $vijest['id'] . '").onclick = function(){ location.href=`' . $clanak_url . '`};</script>';
 						$count++;
 						if($count === $br_clanaka) {
 							break;
@@ -38,17 +40,19 @@ $br_clanaka = 3;
     <div class="section-content">
         <ul class="articles">
 			<div class="section-heading">
-				<h2 class="section-title text"><a href="?page=clanci&kat=sport" class="heading-link">Sport</a></h2>
+				<h2 class="section-title"><a href="?page=clanci&kat=sport" class="heading-link">Sport</a></h2>
 			</div>
 			<?php 
 				$clanci = $clanak->getClanci('sport'); 
 
 				if(!empty($clanci)) {
 					$count = 0; 
-					foreach ($clanci as $vijest) { 
-						echo "<li>" ;
+					foreach ($clanci as $vijest) {
+						$clanak_url = "?page=clanci&kat=" . $vijest['kategorija'] . "&cid=" . $vijest['id'];
+						echo "<li id='" . $vijest['kategorija'] . $vijest['id'] ."' style='cursor: pointer'>";
 						$clanak->ispis_clanka_front($vijest);
 						echo "</li>";
+						echo '<script>document.getElementById("' . $vijest['kategorija'] . $vijest['id'] . '").onclick = function(){ location.href=`' . $clanak_url . '`};</script>';
 						$count++;
 						if($count === $br_clanaka) {
 							break;
